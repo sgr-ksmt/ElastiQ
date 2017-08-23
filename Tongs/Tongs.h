fe9b182ba4e019d02e4a339cc0065c5633f4d180
sgr-ksmt <melodydance.k.s@gmail.com>
2017-08-23 14:12:48 +0900

    Initial setup.

---
+++ Tongs/Tongs.h
@@ -0,0 +1,19 @@
+//
+//  Tongs.h
+//  Tongs
+//
+//  Created by suguru-kishimoto on 2017/08/23.
+//  Copyright © 2017年 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tongs.
+FOUNDATION_EXPORT double TongsVersionNumber;
+
+//! Project version string for Tongs.
+FOUNDATION_EXPORT const unsigned char TongsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tongs/PublicHeader.h>
+
+

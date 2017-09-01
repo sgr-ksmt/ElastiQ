78327400c3349f8e96be8d31f661296ecc29dbc1
sgr-ksmt <melodydance.k.s@gmail.com>
2017-09-01 15:36:01 +0900

    Added Boost.

--- Sources/FunctionalScore.swift
+++ Sources/FunctionalScore.swift
@@ -12,8 +12,23 @@ public final class FunctionalScore: QueryParameter, HaveMultipleParameters {
     public let parameterName: String = "functional_score"
 
     public var parameters: [QueryParameter] = []
-
+    
     public var body: Any {
         return [:]
+    }
+}
+
+extension FunctionalScore {
+    public struct Parameter {
+        private init() {}
+
+        public struct Boost: QueryParameter {
+            public let parameterName: String = "boost"
+            let value: QueryNumberValue
+
+            public var body: Any {
+                return value
+            }
+        }
     }
 }

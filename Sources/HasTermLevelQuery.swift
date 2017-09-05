e8669b3a047df6fe9200b8b8e195c829ad5e5691
sgr-ksmt <melodydance.k.s@gmail.com>
2017-09-05 16:41:16 +0900

    Defined QueryKey and removed dupulicated functions.

--- Sources/HasTermLevelQuery.swift
+++ Sources/HasTermLevelQuery.swift
@@ -13,45 +13,25 @@ public protocol HasTermLevelQuery {
 
 extension HasTermLevelQuery where Self: HasParameter {
     @discardableResult
-    public func term(_ key: String, _ value: QueryValue) -> Self {
-        add(ElastiQ.TermLevelQueries.Term(key: key, value: value))
+    public func term(_ key: QueryKey, _ value: QueryValue) -> Self {
+        add(ElastiQ.TermLevelQueries.Term(key: key.key, value: value))
         return self
     }
 
     @discardableResult
-    public func term<K, V: QueryValue>(_ keypath: PartialKeyPath<K>, _ value: V) -> Self {
-        return term(keypath._kvcKeyPathString!, value)
-    }
-
-    @discardableResult
-    public func terms(_ key: String, _ values: [QueryValue]) -> Self {
-        add(ElastiQ.TermLevelQueries.Terms(key: key, values: values))
+    public func terms(_ key: QueryKey, _ values: [QueryValue]) -> Self {
+        add(ElastiQ.TermLevelQueries.Terms(key: key.key, values: values))
         return self
     }
 
     @discardableResult
-    public func terms<K, V: QueryValue>(_ keypath: PartialKeyPath<K>, _ values: [V]) -> Self {
-        return terms(keypath._kvcKeyPathString!, values)
+    public func range(_ key: QueryKey, _ condition: ElastiQ.TermLevelQueries.Range.Condition) -> Self {
+        return range(key.key, [condition])
     }
 
     @discardableResult
-    public func range(_ key: String, _ condition: ElastiQ.TermLevelQueries.Range.Condition) -> Self {
-        return range(key, [condition])
-    }
-
-    @discardableResult
-    public func range<K>(_ keypath: PartialKeyPath<K>, _ condition: ElastiQ.TermLevelQueries.Range.Condition) -> Self {
-        return range(keypath._kvcKeyPathString!, condition)
-    }
-
-    @discardableResult
-    public func range(_ key: String, _ conditions: [ElastiQ.TermLevelQueries.Range.Condition]) -> Self {
-        add(ElastiQ.TermLevelQueries.Range(key: key, conditions: conditions))
+    public func range(_ key: QueryKey, _ conditions: [ElastiQ.TermLevelQueries.Range.Condition]) -> Self {
+        add(ElastiQ.TermLevelQueries.Range(key: key.key, conditions: conditions))
         return self
-    }
-
-    @discardableResult
-    public func range<K>(_ keypath: PartialKeyPath<K>, _ conditions: [ElastiQ.TermLevelQueries.Range.Condition]) -> Self {
-        return range(keypath._kvcKeyPathString!, conditions)
     }
 }
--- Sources/Value.swift
+++ Sources/Value.swift
@@ -10,9 +10,24 @@ import Foundation
 
 public protocol QueryValue {}
 public protocol QueryNumberValue {}
+public protocol QueryKey {
+    var key: String { get }
+}
 
 extension Int: QueryValue, QueryNumberValue {}
 extension UInt: QueryValue, QueryNumberValue {}
 extension Float: QueryValue, QueryNumberValue {}
 extension Double: QueryValue, QueryNumberValue {}
 extension String: QueryValue {}
+
+extension String: QueryKey {
+    public var key: String {
+        return self
+    }
+}
+
+extension PartialKeyPath: QueryKey {
+    public var key: String {
+        return self._kvcKeyPathString!
+    }
+}
